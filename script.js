53109e2e2876f1e1ef9f8aeaad89ef008fec261c
Daffie989989 <126651209+DavidNaderer01@users.noreply.github.com>
2023-10-30 12:35:56 +0100

    create standart

---
+++ script.js
@@ -0,0 +1,18 @@
+function Sort(content) {
+    var list = "";
+    fetch("link.json")
+        .then(response => response.json())
+        .then(data => {
+            for(let i = 0; i < data.links.length; i++) {
+                let tags = data.links[i].tags;
+
+                tags.forEach(function (tag) {
+                    if(tag == content) {
+                        let url = data.links[i].url;
+                        list += `<li><a href=${url}>${data.links[i].name}</a></li>`
+                    }
+                })
+            }
+            document.getElementById('ul-output').innerHTML = list;
+        });
+}
